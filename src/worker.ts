fab71da42f4508501fe2a39d2decd5ae29793d27
Caleb Norton <n0603919@outlook.com>
2025-07-31 11:09:37 -0500

    just log everything

--- src/worker.ts
+++ src/worker.ts
@@ -5,5 +5,19 @@ interface Environment { }
 export default {
   async email(message: ForwardableEmailMessage, env: Environment, ctx: ExecutionContext) {
     console.log(`Received email for ${message.to}`);
+    message.headers.forEach((value, key) => {
+      console.log(`${key}: ${value}`);
+    });
+    // log the raw email body
+    // await the raw readableStream
+    const reader = message.raw.getReader();
+    console.log("Raw email body:");
+    let result = '';
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      result += new TextDecoder().decode(value);
+    }
+    console.log(result);
   }
 }
